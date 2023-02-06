3f35bdb857345752207b3854ffdeea7930de5edc
Xecozz <94707887+Xecozz@users.noreply.github.com>
2023-02-06 18:23:03 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace GameEngine // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
